2f809f638591cbee851cd6c09ee96b57dc4d6f0e
Richard Armuelles <alexanderarmuelles@hotmail.com>
2016-11-19 22:48:11 -0500

    problemas con socket.io

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ var users = require('./routes/users');
 
 var app = express();
 
+var io = require("socket.io")(app);
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.use('/client', express.static(__dirname + '/node_modules'));
