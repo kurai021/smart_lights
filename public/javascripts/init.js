b6ea5a601619a2e07b3ec45c3431cc4b6b018ecc
Richard Armuelles <alexanderarmuelles@hotmail.com>
2016-11-19 23:31:15 -0500

    arreglando socket

--- public/javascripts/init.js
+++ public/javascripts/init.js
@@ -1,7 +1,7 @@
-var socket = io('//localhost:3000');
+var socket = io('//192.168.0.7:3000');
 
 artyom.initialize({
-    continuous:true,
+    continuous:false,
     lang:"es-ES",
     obeyKeyword: "chip",
     listen:true,
